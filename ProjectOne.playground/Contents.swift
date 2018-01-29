392a924acf3d37d2f1d7707c43766ccea7468f36
Courtney Q <cmquiamz@hawaii.edu>
2018-01-29 12:23:32 -1000

    Complete ProjectOne

---
+++ ProjectOne.playground/Contents.swift
@@ -0,0 +1,35 @@
+//Variable Coding Challenge #1
+
+var firstString = "Aloha"
+let secondString = " Hawaii"
+var thirdString = firstString + secondString
+print(thirdString)
+
+
+//Variable Coding Challenge #2
+
+let celsius: Float
+let fahrenheit: Float = 50
+celsius = fahrenheit - 32 * (5/9)
+print("Today is \(celsius) degrees")
+
+
+//Boolean Variables Coding Challenge #1
+
+var numberOne = 2
+var numberTwo = 5
+var numberThree = numberOne == numberTwo
+
+
+//If...else statement Coding Challenge #1
+var stringOne = "ekahi"
+var stringTwo = "two"
+if stringOne.characters.count > stringTwo.characters.count {
+    
+  print("Hello World")
+}
+else{ print("Hi World")}
+
+
+
+
